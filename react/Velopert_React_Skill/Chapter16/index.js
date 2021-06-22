960e8a62241b31fc3a3777da0a526dce8eec8280
Chamy619 <yd95069506@naver.com>
2021-06-22 22:25:27 +0900

    2021.06.22  - 우아한 테크러닝 7회차  - 리액트를 다루는 기술 16장

---
+++ react/Velopert_React_Skill/Chapter16/index.js
@@ -0,0 +1,62 @@
+import { createStore } from "redux";
+import "./styles.css";
+
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = () => ({ type: INCREASE });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return { ...state, toggle: !state.toggle };
+    case INCREASE:
+      return { ...state, counter: state.counter + 1 };
+    case DECREASE:
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase());
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
